8fd74ea07786852f25d9fdbe4679fe79cd20eb2c
Charles Tabor <chastabor@gmail.com>
2017-11-07 09:50:54 -0600

    Refactor handler as a closer match to CAS, start of request path whitelist filter

--- src/main.rs
+++ src/main.rs
@@ -35,49 +35,22 @@ impl<'a> Service for Router<'a> {
     fn call(&self, req: Request) -> Self::Future {
         futures::future::ok(
             match (req.method(), req.path()) {
+                // Health checks. TODO: Verify key files may be accessible
                 (&Get, "/health") => {
                     Response::new()
                         .with_header(ContentType(mime::TEXT_PLAIN))
                         .with_header(ContentLength(INDEX.len() as u64))
                         .with_body(INDEX)
                 }
-                (&Get, "/") | (&Get, "/get_cookie") => {
-                    let mut body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>get cookie</title></head><body>No id cookie found.</body>".to_string();
-                    if let Some(c) = cookie::Cookie::from_request(&req, Some(cookie::CookiePrefix::HOST), "id") {
-                        if let Ok(id) = c.get_value(Some(self.key)) {
-                            body ="<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>get cookie</title></head><body>id = ".to_string() + &id[..] + "</body>";
-                        } else {
-                            body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>get cookie</title></head><body>Error retrieving id cookie.</body>".to_string();
-                        }
-                    }
+                (&Get, "/static") => {
+                    // CSS and image resource files
+                    let body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>files</title></head><body>This will be replaced with resource file headers and content</body>";
                     Response::new()
                         .with_header(ContentLength(body.len() as u64))
                         .with_body(body)
                 }
-                (&Get, "/set_cookie") => {
-                    if let Ok(mut c) = cookie::Cookie::new(Some(cookie::CookiePrefix::HOST), "id", "test-id", Some(self.key)) {
-                        let body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>set cookie</title></head><body>set id cookie.</body>";
-                        Response::new()
-                            .with_header(ContentLength(body.len() as u64))
-                            .with_header(
-                                hyper::header::SetCookie(vec![
-                                    c.with_path(Some("/"))
-                                        .with_secure(true)
-                                        .with_http_only(true)
-                                        .with_same_site(Some(cookie::SameSite::LAX))
-                                        .get_full_value()
-                                ])
-                            )
-                            .with_body(body)
-                    } else {
-                        let body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>set cookie</title></head><body>Unable to set id cookie.</body>";
-                        Response::new()
-                            .with_header(ContentLength(body.len() as u64))
-                            .with_body(body)
-                    }
-                }
-                (&Get, "/clear_cookie") => {
-                    if let Ok(mut c) = cookie::Cookie::new(Some(cookie::CookiePrefix::HOST), "id", "", Some(self.key)) {
+                (&Get, "/logout") => {
+                    if let Ok(c) = cookie::Cookie::new(Some(cookie::CookiePrefix::HOST), "id", "", Some(self.key)) {
                         let body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>clear cookie</title></head><body>Cleared id cookie.</body>";
                         Response::new()
                             .with_header(ContentLength(body.len() as u64))
@@ -99,16 +72,78 @@ impl<'a> Service for Router<'a> {
                             .with_body(body)
                     }
                 }
-                (&Get, "/files") => {
-                    let body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>files</title></head><body>This will be replaced with file header and content</body>";
-                    Response::new()
-                        .with_header(ContentLength(body.len() as u64))
-                        .with_body(body)
+                (&Get, "/") | (&Get, "/get_set_cookie") => {
+                    if let Some(c) = cookie::Cookie::from_request(&req, Some(cookie::CookiePrefix::HOST), "id") {
+                        // Cookie was found
+                        let body = if let Ok(id) = c.get_value(Some(self.key)) {
+                            "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>get cookie</title></head><body>id = ".to_string() + &id[..] + "</body>"
+                        } else {
+                            "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>get cookie</title></head><body>Error retrieving id cookie.</body>".to_string()
+                        };
+                        Response::new()
+                            .with_header(ContentLength(body.len() as u64))
+                            .with_body(body)
+                    } else { 
+                        // No cookie was found
+                        if let Ok(c) = cookie::Cookie::new(Some(cookie::CookiePrefix::HOST), "id", "test-id", Some(self.key)) {
+                            let body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>set cookie</title></head><body>set id cookie.</body>";
+                            Response::new()
+                                .with_header(ContentLength(body.len() as u64))
+                                .with_header(
+                                    hyper::header::SetCookie(vec![
+                                        c.with_path(Some("/"))
+                                            .with_secure(true)
+                                            .with_http_only(true)
+                                            .with_same_site(Some(cookie::SameSite::LAX))
+                                            .get_full_value()
+                                    ])
+                                )
+                                .with_body(body)
+                        } else {
+                            let body = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title>set cookie</title></head><body>Unable to set id cookie.</body>";
+                            Response::new()
+                                .with_header(ContentLength(body.len() as u64))
+                                .with_body(body)
+                        }
+                    }
                 }
                 _ => Response::new().with_status(StatusCode::NotFound),
-            })
+            }
+        )
     }
 }
+
+// Make sure to leave out directory structures such as:
+//   "/", "..", and hidden paths that start with '.' character.
+// May also want not include bash commands such as:
+//    <, >, ;, *, [, ], (, ), #, !, {, }, ...
+#[inline(always)]
+fn not_whitelist_char(c: char) -> bool {
+    !(c.is_alphanumeric() || c == '_' || c == '-' || c == '.')
+}
+
+/// Whitelist allowed characters, remove .. within
+/// percent decoded path segments.
+fn paths(path: &str) -> Result<Vec<String>, &'static str> {
+    let mut paths = Vec::new();
+    let segments = path.split("/");
+    for segment in segments {
+        let decoded = percent_encoding::percent_decode(segment.as_bytes())
+            .decode_utf8()
+            .map_err(|_| "Invalid percent decoded value in path.")?;
+        if decoded == ".." {
+            paths.pop();
+        } else if decoded.starts_with('.') {
+            return Err("Path contains hidden directory.")
+        } else if decoded.contains(not_whitelist_char) {
+            return Err("Path contains a non-whitelisted character.")
+        } else {
+            paths.push(decoded.to_string());
+        }
+    }
+    Ok(paths)
+}
+
 
 fn load_certs(filename: &str) -> Vec<rustls::Certificate> {
     let certfile = std::fs::File::open(filename).expect("cannot open certificate file");
